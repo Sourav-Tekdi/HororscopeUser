9f23d070fbf17e99f556c7e14ab47f66b4f5a26e
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2025-05-06 23:52:28 +0530

    add

--- my-simple-react-app/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the React application
-npm run build
-
-# Deploy to Hostinger server
-rsync -avz --delete build/ root@69.62.78.171:/var/www/html/
-
-echo "Deployment completed!" 
