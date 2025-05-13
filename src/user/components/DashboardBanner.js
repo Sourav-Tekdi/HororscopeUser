227097c93fdfcf584496b11683ffe4c6a8564261
souravbhowmik1999 <sourav.bhowmik@tekditechnologies.com>
2025-05-13 14:27:02 +0530

    testAutoDeployment

--- src/user/components/DashboardBanner.js
+++ src/user/components/DashboardBanner.js
@@ -1,20 +1,20 @@
 import React from 'react';
 const bannerData = [
     {
-        title: "Trust the Stars",
+        title: "Trust the Starsaa",
         heading: "Take control of your professional destiny",
         description: "Astrology has guided countless individuals in understanding their unique strengths and weaknesses. Let the stars illuminate your path toward success.",
         buttonLabel: null,
     },
     {
-        title: "What’s Your Sign?",
+        title: "What’s Your Signaa?",
         heading: "Discover Your Daily Horoscope",
         description: "Explore how today's celestial movements will influence your sign. Let astrology guide your decisions and help you stay aligned with your true purpose.",
         buttonLabel: "Chat with astrologer",
         buttonLink: "/astrologer",
     },
     {
-        title: "Personalized Guidance",
+        title: "Personalized Guidanceaa",
         heading: "Book an Appointment with Our Astrologers",
         description: "Consult with our professional astrologers to gain insights into your life's direction and make decisions based on the wisdom of the stars.",
         buttonLabel: "Chat with astrologer",
